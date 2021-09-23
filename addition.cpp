7eb541c274a750547af27d261131bc87635f5226
priyansu04 <91240406+priyansu04@users.noreply.github.com>
2021-09-23 10:34:38 +0530

    Addition Program

---
+++ addition.cpp
@@ -0,0 +1,21 @@
+//addition of two numbers
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int price1=100;
+	int price2=80;
+	//int price1,price2;
+	int tprice=price1+price2;
+	int sub = price1 - price2 ; 
+	//tprice=price1+price2;
+	printf("The total price is Rs%d",tprice);
+	printf("\nThe difference in  price is Rs%d",sub);
+	getch();
+	return 0;
+	
+	
+	
+}
+
+ 
